b06993f014237203e19f3789f275b7a796034e9c
Michael Azcueta <azcuetajjmt@gmail.com>
2025-02-20 19:38:29 +0800

    Add more navbar styles

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -31,17 +31,23 @@ onBeforeMount(() => {
 <template>
   <header class="border-b-1 border-b-neutral-300 dark:border-b-neutral-700 py-4 px-5">
     <nav class="flex justify-between items-center text-neutral-800 dark:text-neutral-300">
-      <RouterLink to="/" class="flex items-center font-medium text-xl hover:opacity-70 transition-opacity duration-400">
-        <img alt="Vue logo" class="mr-3" src="@/assets/logo.svg" width="30" height="20" />YGO ODB
+      <RouterLink to="/"
+        class="rounded-xs focus-visible:outline-2 focus-visible:outline-solid focus-visible:outline-offset-2 focus-visible:outline-emerald-500 dark:focus-visible:outline-emerald-700">
+        <div class="flex items-center font-medium text-xl hover:opacity-70 transition-opacity duration-400">
+          <img alt="Vue logo" class="mr-3" src="@/assets/logo.svg" width="30" height="20" />YGO ODB
+        </div>
       </RouterLink>
       <div class="flex items-center divide-x-1 divide-neutral-300 dark:divide-neutral-700">
-        <RouterLink class="pr-4 font-medium hover:text-emerald-600 duration-400" activeClass="text-emerald-600"
-          to="/about">About
-        </RouterLink>
+        <div class="pr-4">
+          <RouterLink
+            class="font-medium hover:text-emerald-600 duration-400 rounded-xs transition-text-color focus-visible:outline-2 focus-visible:outline-solid focus-visible:outline-offset-4 focus-visible:outline-emerald-500 dark:focus-visible:outline-emerald-700"
+            activeClass="text-emerald-600" to="/about">About
+          </RouterLink>
+        </div>
         <SwitchRoot id="switch-theme" v-model:checked="switchState" @update:checked="switchTheme"
-          class="w-[42px] h-[22px] ml-4 border border-solid focus:outline-none focus-visible:border-emerald-600 duration-400 border-neutral-400 dark:border-neutral-500 hover:border-emerald-600 bg-neutral-200 shadow-sm rounded-full relative data-[state=checked]:bg-neutral-700 cursor-pointer">
+          class="w-[42px] h-[22px] ml-4 border transition-border-color duration-400 border-neutral-400 dark:border-neutral-500 hover:border-emerald-600 bg-neutral-200 shadow-sm rounded-full relative data-[state=checked]:bg-neutral-700 cursor-pointer focus-visible:outline-2 focus-visible:outline-solid focus-visible:-outline-offset-2 focus-visible:outline-emerald-500 dark:focus-visible:outline-emerald-700">
           <SwitchThumb
-            class="flex justify-center items-center w-[18px] h-[18px] my-auto bg-white shadow-sm rounded-full transition-transform duration-300 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[20.5px] data-[state=checked]:bg-neutral-950">
+            class="flex justify-center items-center w-[18px] h-[18px] my-auto bg-white shadow-sm rounded-full transition-transform duration-300 translate-x-[1px] will-change-transform data-[state=checked]:translate-x-[21px] data-[state=checked]:bg-neutral-950">
             <Sun v-if="!switchState" color="gray" :size="14" />
             <Moon v-else color="white" :size="14" />
           </SwitchThumb>
