9ff90dc6cc96b9d89114a1b59ff595bf8c5bdadb
Michael Azcueta <azcuetajjmt@gmail.com>
2025-02-21 15:43:30 +0800

    fix toggle theme v-model and update navbar styles

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -31,8 +31,7 @@ onBeforeMount(() => {
 <template>
   <header class="border-b-1 border-b-neutral-300 dark:border-b-neutral-700 py-4 px-5">
     <nav class="flex justify-between items-center text-neutral-800 dark:text-neutral-300">
-      <RouterLink to="/"
-        class="rounded-xs focus-visible:outline-2 focus-visible:outline-solid focus-visible:outline-offset-2 focus-visible:outline-emerald-500 dark:focus-visible:outline-emerald-700">
+      <RouterLink to="/" class="focus-visible:outline-offset-2">
         <div class="flex items-center font-medium text-xl hover:opacity-70 transition-opacity duration-400">
           <img alt="Vue logo" class="mr-3" src="@/assets/logo.svg" width="30" height="20" />YGO ODB
         </div>
@@ -40,12 +39,12 @@ onBeforeMount(() => {
       <div class="flex items-center divide-x-1 divide-neutral-300 dark:divide-neutral-700">
         <div class="pr-4">
           <RouterLink
-            class="font-medium hover:text-emerald-700 dark:hover:text-emerald-600 duration-400 rounded-xs transition-text-color focus-visible:outline-2 focus-visible:outline-solid focus-visible:outline-offset-4 focus-visible:outline-emerald-500 dark:focus-visible:outline-emerald-700"
+            class="font-medium hover:text-emerald-700 dark:hover:text-emerald-600 transition-[color] duration-400 focus-visible:outline-offset-4"
             activeClass="text-emerald-700 dark:text-emerald-600" to="/about">About
           </RouterLink>
         </div>
-        <SwitchRoot id="switch-theme" v-model:checked="switchState" @update:checked="switchTheme"
-          class="w-[42px] h-[22px] ml-4 border transition-border-color duration-400 border-neutral-400 dark:border-neutral-500 hover:border-emerald-600 bg-neutral-200 shadow-sm rounded-full relative data-[state=checked]:bg-neutral-700 cursor-pointer focus-visible:outline-2 focus-visible:outline-solid focus-visible:-outline-offset-2 focus-visible:outline-emerald-500 dark:focus-visible:outline-emerald-700">
+        <SwitchRoot id="switch-theme" v-model:modelValue="switchState" @update:modelValue="switchTheme"
+          class="w-[42px] h-[22px] ml-4 shadow-sm rounded-full relative cursor-pointer border duration-400 border-neutral-400 dark:border-neutral-500 hover:border-emerald-600 bg-neutral-200 data-[state=checked]:bg-neutral-700">
           <SwitchThumb
             class="flex justify-center items-center w-[18px] h-[18px] my-auto bg-white shadow-sm rounded-full transition-transform duration-300 translate-x-[1px] will-change-transform data-[state=checked]:translate-x-[21px] data-[state=checked]:bg-neutral-950">
             <Sun v-if="!switchState" color="gray" :size="14" />
