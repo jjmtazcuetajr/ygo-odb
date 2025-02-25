6ec88daaa2af167ac3cab76c6f54f4159a888249
Michael Azcueta <azcuetajjmt@gmail.com>
2025-02-25 15:42:09 +0800

    add some controls (no functionality yet)

---
+++ src/components/DropdownMenu.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { FileInput, FileOutput, ChevronDown, ArrowDownUp } from 'lucide-vue-next';
+import { DropdownMenuArrow, DropdownMenuContent, DropdownMenuItem, DropdownMenuPortal, DropdownMenuRoot, DropdownMenuTrigger } from 'reka-ui'
+
+defineProps<{
+  type: 'Import' | 'Export' | 'Sort'
+}>()
+
+const toggleState = ref(false)
+
+function importFromYdkFile() {
+  console.log('import from ydk file');
+}
+function importFromYdkeUrl() {
+  console.log('import from ydke url');
+}
+function exportToYdkFile() {
+  console.log('export to ydk file');
+}
+function exportToYdkeUrl() {
+  console.log('export to ydke url');
+}
+function sortByName() {
+  console.log('sort by name');
+}
+function sortByArchetype() {
+  console.log('sort by archetype');
+}
+</script>
+
+<template>
+  <DropdownMenuRoot v-model:open="toggleState" :modal="false">
+    <DropdownMenuTrigger
+      class="flex place-items-center px-2 py-1 rounded-md cursor-pointer text-xs sm:text-base dark:text-white bg-gray-200 hover:bg-gray-300 active:bg-gray-400 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-500 transition-[background-color,color] duration-200"
+      aria-label="Import options">
+      <FileInput v-if="type === 'Import'" class="mr-1" :size="16" />
+      <FileOutput v-else-if="type === 'Export'" class="mr-1" :size="16" />
+      <ArrowDownUp v-else class="mr-1" :size="16" />
+      {{ type }}
+      <ChevronDown class="ml-1" :size="16" />
+    </DropdownMenuTrigger>
+
+    <DropdownMenuPortal>
+      <DropdownMenuContent
+        class="rounded-md p-1 border border-neutral-300 dark:border-neutral-600 bg-neutral-100 dark:bg-neutral-800 shadow-xl shadow-neutral-400 dark:shadow-neutral-800 will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+        :side-offset="5">
+        <template v-if="type === 'Import' || type === 'Export'">
+          <DropdownMenuItem v-on="type === 'Import' ? { click: importFromYdkFile } : { click: exportToYdkFile }"
+            class="text-sm rounded flex items-center h-6 px-3 select-none outline-none text-emerald-700 data-[highlighted]:bg-emerald-500 data-[highlighted]:text-neutral-50 dark:text-emerald-400">
+            {{ type === 'Import' ? 'From' : 'To' }} .ydk file
+          </DropdownMenuItem>
+          <DropdownMenuItem v-on="type === 'Import' ? { click: importFromYdkeUrl } : { click: exportToYdkeUrl }"
+            class="text-sm rounded flex items-center h-6 px-3 select-none outline-none text-emerald-700 data-[highlighted]:bg-emerald-500 data-[highlighted]:text-neutral-50 dark:text-emerald-400">
+            {{ type === 'Import' ? 'From' : 'To' }} YDKe URL
+          </DropdownMenuItem>
+        </template>
+        <template v-else>
+          <DropdownMenuItem @click="sortByName"
+            class="text-sm rounded flex items-center h-6 px-3 select-none outline-none text-emerald-700 data-[highlighted]:bg-emerald-500 data-[highlighted]:text-neutral-50 dark:text-emerald-400">
+            By name
+          </DropdownMenuItem>
+          <DropdownMenuItem @click="sortByArchetype"
+            class="text-sm rounded flex items-center h-6 px-3 select-none outline-none text-emerald-700 data-[highlighted]:bg-emerald-500 data-[highlighted]:text-neutral-50 dark:text-emerald-400">
+            By archetype
+          </DropdownMenuItem>
+        </template>
+        <DropdownMenuArrow class="fill-neutral-100 dark:fill-neutral-800 stroke-neutral-300 dark:stroke-neutral-600" />
+      </DropdownMenuContent>
+    </DropdownMenuPortal>
+  </DropdownMenuRoot>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,31 @@
 <script setup lang="ts">
 import DeckType from '@/components/DeckType.vue';
+import DropdownMenu from '@/components/DropdownMenu.vue';
+import { Trash2, CircleHelp } from 'lucide-vue-next';
 </script>
 <template>
-  <main class="p-5 dark:text-neutral-300">
-    <div class="flex justify-between">
+  <main class="p-5 dark:text-neutral-300 transition-[color] duration-400">
+    <div class="flex justify-between flex-wrap gap-3">
       <div>
         <h2 class="text-3xl font-medium">YGO ODB</h2>
         <h6 class="text-sm">An online, fan-made Yu-Gi-Oh! deck builder</h6>
       </div>
-      <div>
-        <button type="button"
-          class="px-2 py-1 rounded-md cursor-pointer bg-gray-200 hover:bg-gray-300 active:bg-gray-400 dark:bg-zinc-800 dark:hover:bg-zinc-700 dark:active:bg-zinc-600 duration-400">Import</button>
-        <!-- <button type="button" class="px-2 border border-transparent hover:border-emerald-500 focus:outline-none focus:border-emerald-500 focus:shadow-emerald transition-border-shadow duration-200">Import</button> -->
+      <div class="flex gap-2 place-items-center flex-wrap">
+        <DropdownMenu type="Import" />
+        <DropdownMenu type="Export" />
+        <DropdownMenu type="Sort" />
+        <div class="flex gap-2">
+          <button type="button"
+            class="flex place-items-center px-2 py-1 rounded-md cursor-pointer text-xs sm:text-base dark:text-white bg-gray-200 hover:bg-gray-300 active:bg-gray-400 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-500 transition-[background-color,color] duration-200">
+            <Trash2 class="mr-1" :size="16" />
+            Clear
+          </button>
+          <button type="button"
+            class="flex place-items-center px-2 py-1 rounded-md cursor-pointer text-xs sm:text-base dark:text-white bg-gray-200 hover:bg-gray-300 active:bg-gray-400 dark:bg-zinc-700 dark:hover:bg-zinc-600 dark:active:bg-zinc-500 transition-[background-color,color] duration-200">
+            <CircleHelp class="mr-1" :size="16" />
+            Help
+          </button>
+        </div>
       </div>
     </div>
     <div class="flex gap-4 mt-3">
