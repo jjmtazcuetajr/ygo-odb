8b57b266723699887f564df4bce02f67f4ff53c5
Michael Azcueta <azcuetajjmt@gmail.com>
2025-03-04 20:27:12 +0800

    Add controls and placeholder images to filter panel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import DeckType from '@/components/DeckType.vue';
 import DropdownMenu from '@/components/DropdownMenu.vue';
-import { Trash2, CircleHelp, Search, X } from 'lucide-vue-next';
+import { Trash2, CircleHelp, Search, X, Filter } from 'lucide-vue-next';
 import { ref, onMounted } from 'vue';
 
 const isMobileFiltersShown = ref(false)
@@ -58,14 +58,46 @@ onMounted(() => {
         <DeckType type="Side" />
       </div>
       <transition name="slide-fade">
-        <div id="mobile-filters" v-if="isMobileFiltersShown"
+        <div v-if="isMobileFiltersShown"
           class="fixed sm:static inset-0 mt-[60px] sm:mt-[unset] sm:w-[40%] lg:w-[30%] xl:w-[25%]">
           <div
-            class="relative bg-neutral-100 dark:bg-neutral-800 border-r sm:border-r-0 border-y sm:border-y-0 rounded-r-lg sm:rounded-r-none border-neutral-400 dark:border-neutral-500 shadow-[5px_15px_15px_5px_#999] dark:shadow-[5px_15px_15px_5px_#000] sm:shadow-[unset] dark:sm:shadow-[unset] w-[70%] sm:w-full h-full transition-[background-color,border-color,box-shadow] duration-400">
-            <button type="button" aria-label="Hide filters" @click="toggleMobileFilters"
-              class="absolute right-0 m-2 flex sm:hidden place-items-center p-2 rounded-full cursor-pointer text-xs sm:text-base dark:text-white bg-gray-200 active:bg-gray-400 dark:bg-zinc-700 dark:active:bg-zinc-500 transition-[background-color,color] duration-200">
-              <X :size="16" />
-            </button>
+            class="flex flex-col gap-2 p-3 bg-neutral-100 dark:bg-neutral-800 border-r sm:border-r-0 border-y sm:border-y-0 rounded-r-lg sm:rounded-r-none border-neutral-400 dark:border-neutral-500 shadow-[5px_15px_15px_5px_#999] dark:shadow-[5px_15px_15px_5px_#000] sm:shadow-[unset] dark:sm:shadow-[unset] w-[70%] sm:w-full h-full transition-[background-color,border-color,box-shadow] duration-400">
+            <div class="flex sm:hidden items-center">
+              <span class="text-lg leading-none font-medium grow">Search and filter cards</span>
+              <button type="button" aria-label="Hide filters" @click="toggleMobileFilters"
+                class="sm:hidden self-start p-1 size-[24px] rounded-full cursor-pointer text-xs sm:text-base dark:text-white bg-gray-200 active:bg-gray-400 dark:bg-zinc-700 dark:active:bg-zinc-500 transition-[background-color,color] duration-200">
+                <X :size="16" />
+              </button>
+            </div>
+            <input type="text" placeholder="Enter card name or effect..."
+              class="w-full text-sm sm:text-base rounded-md px-2 py-0.5 placeholder:italic placeholder:text-neutral-400 border border-neutral-500 bg-neutral-50 dark:bg-neutral-900 transition-[background-color] duration-400">
+            <div class="flex flex-wrap gap-2">
+              <select aria-label="Sort by"
+                class="text-xs sm:text-base rounded-md px-1 py-0.5 border border-neutral-500 bg-neutral-50 dark:bg-neutral-900 transition-[background-color] duration-400">
+                <option selected disabled>Sort by</option>
+                <option value="">Name</option>
+                <option value="">Attack</option>
+                <option value="">Defense</option>
+              </select>
+              <select aria-label="Sort direction"
+                class="text-xs sm:text-base rounded-md px-1 py-0.5 border border-neutral-500 bg-neutral-50 dark:bg-neutral-900 transition-[background-color] duration-400">
+                <option selected disabled>Direction</option>
+                <option value="asc">Ascending</option>
+                <option value="desc">Descending</option>
+              </select>
+              <button type="button"
+                class="flex place-items-center px-2 py-1 rounded-md cursor-pointer text-xs sm:text-base dark:text-white bg-gray-300 hover:bg-gray-400 active:bg-gray-500 dark:bg-zinc-600 dark:hover:bg-zinc-500 dark:active:bg-zinc-400 transition-[background-color,color] duration-200">
+                <Filter class="mr-1" :size="16" />
+                Filters
+              </button>
+            </div>
+            <div
+              class="grid grid-cols-3 xl:grid-cols-4 gap-3 overflow-y-auto grow shrink basis-0 sm:px-2 mt-6 content-start dark:[color-scheme:dark]">
+              <template v-for="_ in 14">
+                <img src="https://images.ygoprodeck.com/images/cards_small/5043010.jpg" alt="Firewall Dragon"
+                  class="rounded-sm aspect-[268/391]">
+              </template>
+            </div>
           </div>
         </div>
       </transition>
