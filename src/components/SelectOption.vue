4e82386cea45461e111f6f4ee06d03ddff216bbd
Michael Azcueta <azcuetajjmt@gmail.com>
2025-03-14 14:32:19 +0800

    more select component customizations

--- src/components/SelectOption.vue
+++ src/components/SelectOption.vue
@@ -1,24 +1,30 @@
 <script setup lang="ts">
 interface SelectOption {
   val: string,
-  textContent: string,
-  selected?: boolean,
-  disabled?: boolean
+  textContent: string
 }
 
 defineProps<{
   id: string,
-  label?: string,
   bgColorClass?: string,
   options: SelectOption[]
 }>()
 </script>
 <template>
-  <select :id="id" :aria-label="label || ''"
-    @change="$emit('handleOptionChange', ($event.target as HTMLSelectElement).value)"
-    class="text-xs sm:text-base rounded-md px-1 py-0.5 border border-neutral-500 transition-[background-color] duration-400"
+  <select :id="id" @change="$emit('handleOptionChange', ($event.target as HTMLSelectElement).value)"
+    class="text-xs sm:text-base rounded-md pl-1.5 pr-6 py-0.5 appearance-none bg-no-repeat bg-[right_.2rem_center] bg-[auto_1.1rem] border border-neutral-500 transition-[background-color,background-image] duration-400"
     :class="bgColorClass || 'bg-neutral-100 dark:bg-neutral-800'">
-    <option v-for="option in options" :key="option.val" :value="option.val" :selected="option.selected || false"
-      :disabled="option.disabled || false">{{ option.textContent }}</option>
+    <option v-for="(option, idx) in options" :key="option.val" :value="option.val" :selected="idx === 0 ? true : false">
+      {{ option.textContent }}
+    </option>
   </select>
 </template>
+<style>
+select {
+  background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLWNoZXZyb24tZG93biI+PHBhdGggZD0ibTYgOSA2IDYgNi02Ii8+PC9zdmc+');
+}
+
+[data-theme="dark"] select {
+  background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiNmZmZmZmYiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIiBjbGFzcz0ibHVjaWRlIGx1Y2lkZS1jaGV2cm9uLWRvd24iPjxwYXRoIGQ9Im02IDkgNiA2IDYtNiIvPjwvc3ZnPg==');
+}
+</style>
