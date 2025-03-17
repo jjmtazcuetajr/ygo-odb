9151411f26f399508e8914de64a8744b648df331
Michael Azcueta <azcuetajjmt@gmail.com>
2025-03-17 16:10:10 +0800

    add link arrow selection

--- src/components/dialog-content/CardFilters.vue
+++ src/components/dialog-content/CardFilters.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 import SelectOption from '../SelectOption.vue';
 import AttributeSelection from './AttributeSelection.vue';
 import NumberField from '../NumberField.vue';
+import LinkArrows from './LinkArrows.vue';
 import { cardCategories, monsterCards, spellTypes, trapTypes, monsterTypes } from '../../utils/select-options'
 
 const category = ref()
@@ -34,6 +35,10 @@ const handleCategoryChange = (val: string) => { category.value = val }
           <NumberField id="lvl-rank" :max="12" label-val="Lv/Rank" />
           <NumberField id="scale" :max="13" label-val="Scale" class="mt-1" />
           <NumberField id="link" :min="1" :max="6" :default-val="1" label-val="Link Rating" class="mt-1" />
+        </div>
+        <div class="mt-2">
+          Link Arrows
+          <LinkArrows class="mt-1" />
         </div>
       </template>
       <template v-else-if="category === 'spell'">
---
+++ src/components/dialog-content/LinkArrows.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ToggleGroupItem, ToggleGroupRoot } from 'reka-ui'
+import { ArrowUp, ArrowDown, ArrowLeft, ArrowRight, ArrowUpLeft, ArrowUpRight, ArrowDownLeft, ArrowDownRight } from 'lucide-vue-next';
+import { ref } from 'vue'
+
+const toggleStateMultiple = ref([])
+
+const toggleButtonStyles = 'cursor-pointer flex justify-center items-center rounded-full size-[24px] shadow-sm data-[state=on]:inset-shadow-sm data-[state=on]:shadow-none transition-[color,background-color,box-shadow] duration-300'
+const lightThemeColors = ' bg-neutral-200 hover:enabled:data-[state=off]:bg-neutral-300 shadow-neutral-400 data-[state=on]:bg-neutral-400 data-[state=on]:inset-shadow-neutral-600'
+const darkThemeColors = ' dark:bg-neutral-700 dark:hover:enabled:data-[state=off]:bg-neutral-600 dark:shadow-neutral-500 dark:data-[state=on]:bg-neutral-500 dark:data-[state=on]:inset-shadow-neutral-900'
+const disabledState = ' disabled:cursor-not-allowed disabled:opacity-70 disabled:shadow-none disabled:text-black/70 dark:disabled:text-neutral-200/70'
+const toggleGroupItemClasses = toggleButtonStyles + lightThemeColors + darkThemeColors + disabledState
+</script>
+
+<template>
+  <ToggleGroupRoot v-model="toggleStateMultiple" type="multiple"
+    class="flex flex-col justify-between size-[100px] dark:text-neutral-200">
+    <div class="flex justify-between">
+      <ToggleGroupItem value="top-left" aria-label="Top-Left Link Arrow" :class="toggleGroupItemClasses">
+        <ArrowUpLeft :size="16" />
+      </ToggleGroupItem>
+      <ToggleGroupItem value="top" aria-label="Top Link Arrow" :class="toggleGroupItemClasses">
+        <ArrowUp :size="16" />
+      </ToggleGroupItem>
+      <ToggleGroupItem value="top-right" aria-label="Top-Right Link Arrow" :class="toggleGroupItemClasses">
+        <ArrowUpRight :size="16" />
+      </ToggleGroupItem>
+    </div>
+    <div class="flex justify-between">
+      <ToggleGroupItem value="left" aria-label="Left Link Arrow" :class="toggleGroupItemClasses">
+        <ArrowLeft :size="16" />
+      </ToggleGroupItem>
+      <ToggleGroupItem value="right" aria-label="Right Link Arrow" :class="toggleGroupItemClasses">
+        <ArrowRight :size="16" />
+      </ToggleGroupItem>
+    </div>
+    <div class="flex justify-between">
+      <ToggleGroupItem value="bottom-left" aria-label="Bottom-Left Link Arrow" :class="toggleGroupItemClasses">
+        <ArrowDownLeft :size="16" />
+      </ToggleGroupItem>
+      <ToggleGroupItem value="bottom" aria-label="Bottom Link Arrow" :class="toggleGroupItemClasses">
+        <ArrowDown :size="16" />
+      </ToggleGroupItem>
+      <ToggleGroupItem value="bottom-right" aria-label="Bottom-Right Link Arrow" :class="toggleGroupItemClasses">
+        <ArrowDownRight :size="16" />
+      </ToggleGroupItem>
+    </div>
+  </ToggleGroupRoot>
+</template>
