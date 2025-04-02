991d5cec7d9a0356d103c4b2f55d014fb03f48a5
Michael Azcueta <azcuetajjmt@gmail.com>
2025-04-02 15:13:57 +0800

    add interfaces

---
+++ src/utils/data-types.ts
@@ -0,0 +1,74 @@
+interface BanlistInfo {
+  ban_ocg?: string,
+  ban_tcg?: string,
+  ban_goat?: string
+}
+
+interface CardImages {
+  id: number,
+  image_url: string,
+  image_url_cropped: string,
+  image_url_small: string
+}
+
+interface CardPrices {
+  amazon_price: string,
+  cardmarket_price: string,
+  coolstuffinc_price: string,
+  ebay_price: string,
+  tcgplayer_price: string
+}
+
+interface CardSets {
+  set_code: string,
+  set_name: string,
+  set_price: string,
+  set_rarity: string,
+  set_rarity_code: string,
+}
+
+interface MiscInfo {
+  downvotes: number,
+  formats: string[],
+  has_effect: number,
+  konami_id: number,
+  md_rarity: string,
+  ocg_date: Date,
+  staple?: string,
+  tcg_date: Date,
+  treated_as: string,
+  upvotes: number,
+  views: number,
+  viewsweek: number
+}
+
+export interface YGOCardData {
+  archetype?: string,
+  atk?: number,
+  attribute?: string,
+  banlist_info?: BanlistInfo,
+  card_images: CardImages[],
+  card_prices?: CardPrices[],
+  card_sets?: CardSets[],
+  def?: number,
+  desc: string,
+  frameType: string,
+  humanReadableCardType: string,
+  id: number,
+  level?: number,
+  linkmarkers?: string[],
+  linkval?: number,
+  misc_info: MiscInfo[],
+  monster_desc?: string,
+  name: string,
+  pend_desc?: string
+  race: string,
+  scale?: number
+  type: string,
+  typeline?: string[],
+  ygoprodeck_url: string
+}
+
+export interface YGOCards {
+  data: YGOCardData[]
+}
