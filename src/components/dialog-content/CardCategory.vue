161f3498a5667443105bb9d58da82eab782c9184
Michael Azcueta <azcuetajjmt@gmail.com>
2025-04-24 15:38:02 +0800

    fix typescript error

--- src/components/dialog-content/CardCategory.vue
+++ src/components/dialog-content/CardCategory.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ToggleGroupItem, ToggleGroupRoot } from 'reka-ui'
 import { useYgoCardsStore } from "@/stores/ygo-cards"
+import type { CardCategory } from "@/utils/interfaces";
 
 const toggleButtonStyles = 'cursor-pointer px-2 py-0.5 rounded-full w-[60px] sm:w-[80px] text-xs sm:text-base shadow-sm data-[state=on]:inset-shadow-sm data-[state=on]:shadow-none transition-[color,background-color,box-shadow] duration-300'
 const lightThemeColors = ' bg-neutral-200 hover:bg-neutral-300 shadow-neutral-400 data-[state=on]:bg-neutral-400 data-[state=on]:inset-shadow-neutral-600'
@@ -12,7 +13,7 @@ const { resetCardCategory } = useYgoCardsStore()
 
 <template>
   <ToggleGroupRoot type="single" class="flex flex-wrap gap-2 dark:text-neutral-200"
-    @update:model-value="(val) => resetCardCategory(val as string)">
+    @update:model-value="(val) => resetCardCategory(val as CardCategory)">
     <ToggleGroupItem value="monster" :class="toggleGroupItemClasses">Monster</ToggleGroupItem>
     <ToggleGroupItem value="spell" :class="toggleGroupItemClasses">Spell</ToggleGroupItem>
     <ToggleGroupItem value="trap" :class="toggleGroupItemClasses">Trap</ToggleGroupItem>
