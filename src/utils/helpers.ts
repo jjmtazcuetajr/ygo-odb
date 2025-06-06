852149e9fe304f90e0727518cdd9da41c6990d39
Michael Azcueta <azcuetajjmt@gmail.com>
2025-06-06 22:53:33 +0800

    remove some card manual corrections because the api fixed them already

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -18,12 +18,8 @@ export function matchCategory(card: YGOCardData, category: CardCategory | undefi
  * @param cardType Monster card type based on frame color
  */
 export function matchMonsterCardType(card: YGOCardData, cardType: string): boolean {
-  // this is needed because Supreme King Z-ARC - Synchro Universe should be a Synchro Pendulum monster, not Synchro only
-  const specificCard = card.id === 48654267
-  const exclude = cardType === 'synchro' && specificCard
-  const include = cardType === 'pendulum' && specificCard
-  if (cardType === 'pendulum') return card.frameType.includes(cardType) || include
-  else if (cardType !== '' && cardType !== 'pendulum') return card.frameType === cardType && !exclude
+  if (cardType === 'pendulum') return card.frameType.includes(cardType)
+  else if (cardType !== '' && cardType !== 'pendulum') return card.frameType === cardType
   return true
 }
 
@@ -51,13 +47,12 @@ export function matchMonsterAbility(card: YGOCardData, ability: string): boolean
       gemini: { typeline: 'Gemini' },
       spirit: {
         typeline: 'Spirit',
-        ids: [53270092, 71614230, 77511331, 52900000, 60823690, 25415052, 33325951, 40516623],
-        // Han-Shi Kyudo Spirit, Kai-Den Kendo Spirit, Kuro-Obi Karate Spirit, Shinobaron Peacock, Shinobaron Shade Peacock, Shinobaroness Peacock, Shinobaroness Shade Peacock, Yoko-Zuna Sumo Spirit
+        ids: [52900000, 60823690, 25415052, 33325951] // Shinobaron Peacock, Shinobaron Shade Peacock, Shinobaroness Peacock, Shinobaroness Shade Peacock
       },
       toon: { typeline: 'Toon' },
       union: {
         typeline: 'Union',
-        ids: [79538761], // Torque Tune Gear
+        ids: [79538761] // Torque Tune Gear
       }
     }
 
@@ -84,8 +79,7 @@ export function matchTunerType(card: YGOCardData, tuner: string): boolean {
     // this is needed because the YGOPRODeck API lacks the 'Tuner' type in the typeline data for these monsters
     const specialTuners: Record<string, number[]> = {
       effect: [16769305], // Turbo-Tainted Hot Rod GT19
-      fusion: [45655875, 66532962], // Magikey Beast - Ansyalabolas, Magistus Chorozo
-      pendulum: [82112494, 41908872, 43210483] // Superheavy Samurai Prodigy Wakaushi, Supreme King Dragon Lightwurm, Symphonic Warrior Guitariss
+      fusion: [45655875, 66532962] // Magikey Beast - Ansyalabolas, Magistus Chorozo
     }
 
     const frameTypeMapping: Record<string, string[]> = {
@@ -101,10 +95,6 @@ export function matchTunerType(card: YGOCardData, tuner: string): boolean {
     const frameMatch = frameTypeMapping[tuner]?.includes(card.frameType)
     const specialTunerMatch = specialTuners[tuner]?.includes(card.id)
     
-    if (tuner === 'effect') {
-      const effectTuners = specialTuners['effect'].concat(specialTuners['pendulum']).includes(card.id)
-      return (isTuner && frameMatch) || effectTuners
-    }
     return (isTuner && frameMatch) || specialTunerMatch
   }
   return true
@@ -117,12 +107,7 @@ export function matchTunerType(card: YGOCardData, tuner: string): boolean {
  * @param pendulum Type of Pendulum Monster card
  */
 export function matchPendulumType(card: YGOCardData, pendulum: string): boolean {
-  if (pendulum !== '') {
-    // this is needed because Supreme King Z-ARC - Synchro Universe isn't labeled as a Synchro Pendulum monster
-    const specialPendulums: Record<string, number[]> = { synchro_pendulum: [48654267] }
-    const isSpecialPendulum = specialPendulums[pendulum]?.includes(card.id)
-    return card.frameType === pendulum || isSpecialPendulum
-  }
+  if (pendulum !== '') return card.frameType === pendulum
   return true
 }
 
@@ -150,8 +135,8 @@ export function matchRank(card: YGOCardData, rank: number): boolean {
  */
 export function matchPendulumScale(card: YGOCardData, scale: number): boolean {
   if (!Number.isNaN(scale)) {
-    // this is needed because the scale of D/D/D Vice King Requiem should be 4, not 8 and Speedroid Wing Synchron has no scale in the YGOPRODeck API
-    const specialPendulumScales: Record<number, number[]> = { 4: [25857977, 2254222] }
+    // this is needed because the scale of D/D/D Vice King Requiem should be 4, not 8
+    const specialPendulumScales: Record<number, number[]> = { 4: [25857977] }
     const isSpecialPendulumScale = specialPendulumScales[scale]?.includes(card.id)
     const exclude = !(scale === 8 && card.id === 25857977)
     return (card.scale === scale && exclude) || isSpecialPendulumScale
@@ -320,11 +305,9 @@ export function sortByMonsterStat(cardA: YGOCardData, cardB: YGOCardData, stat: 
       case 'scale':
         /**
          * Determine if card is a Pendulum Monster
-         * 
-         * **Note**: the card id checking should correctly label `Supreme King Z-ARC - Synchro Universe` as a Pendulum Monster
          * @param card Yu-Gi-Oh! card data
          */
-        const isPendulumMonster = (card: YGOCardData): boolean => card.frameType.includes('pendulum') || card.id === 48654267
+        const isPendulumMonster = (card: YGOCardData): boolean => card.frameType.includes('pendulum')
 
         // Pendulum monsters come first than everything else
         if (isPendulumMonster(cardA) && !isPendulumMonster(cardB)) return -1
