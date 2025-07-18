4d8d925222c4182db9c15db04fc794d44fd1dc5b
Michael Azcueta <azcuetajjmt@gmail.com>
2025-07-18 15:47:14 +0800

    improve html

--- src/components/card-popover/GridToDeck.vue
+++ src/components/card-popover/GridToDeck.vue
@@ -28,36 +28,36 @@ function isExtraDeckCard(cardFrame: string): boolean {
 }
 </script>
 <template>
-  <div v-if="isMainDeckCard(card.frameType)" class="flex flex-col gap-2 dark:text-neutral-300">
+  <div v-if="isMainDeckCard(card.frameType)" class="dark:text-neutral-300">
     <span>To Main Deck:</span>
-    <div class="flex gap-2">
-      <ButtonCTA variant="neutral-2" text-content="+ 1" class="sm:text-sm" :disabled="!isCardWithinLimit(card, 'main')"
-        @click="addCardToDeck(card, mainDeck.length, 'main')" />
-      <ButtonCTA variant="neutral-2" text-content="+ 2" class="sm:text-sm"
+    <div class="flex gap-2 mt-1">
+      <ButtonCTA variant="emerald" text-content="+ 1" aria-label="Add 1 copy"
+        :disabled="!isCardWithinLimit(card, 'main')" @click="addCardToDeck(card, mainDeck.length, 'main')" />
+      <ButtonCTA variant="emerald" text-content="+ 2" aria-label="Add 2 copies"
         :disabled="!isCardWithinLimit(card, 'main', 2)" @click="addCardToDeck(card, mainDeck.length, 'main', 2)" />
-      <ButtonCTA variant="neutral-2" text-content="+ 3" class="sm:text-sm"
+      <ButtonCTA variant="emerald" text-content="+ 3" aria-label="Add 3 copies"
         :disabled="!isCardWithinLimit(card, 'main', 3)" @click="addCardToDeck(card, mainDeck.length, 'main', 3)" />
     </div>
   </div>
-  <div v-else-if="isExtraDeckCard(card.frameType)" class="flex flex-col gap-2 dark:text-neutral-300">
+  <div v-else-if="isExtraDeckCard(card.frameType)" class="dark:text-neutral-300">
     <span>To Extra Deck:</span>
-    <div class="flex gap-2">
-      <ButtonCTA variant="neutral-2" text-content="+ 1" class="sm:text-sm" :disabled="!isCardWithinLimit(card, 'extra')"
-        @click="addCardToDeck(card, extraDeck.length, 'extra')" />
-      <ButtonCTA variant="neutral-2" text-content="+ 2" class="sm:text-sm"
+    <div class="flex gap-2 mt-1">
+      <ButtonCTA variant="emerald" text-content="+ 1" aria-label="Add 1 copy"
+        :disabled="!isCardWithinLimit(card, 'extra')" @click="addCardToDeck(card, extraDeck.length, 'extra')" />
+      <ButtonCTA variant="emerald" text-content="+ 2" aria-label="Add 2 copies"
         :disabled="!isCardWithinLimit(card, 'extra', 2)" @click="addCardToDeck(card, extraDeck.length, 'extra', 2)" />
-      <ButtonCTA variant="neutral-2" text-content="+ 3" class="sm:text-sm"
+      <ButtonCTA variant="emerald" text-content="+ 3" aria-label="Add 3 copies"
         :disabled="!isCardWithinLimit(card, 'extra', 3)" @click="addCardToDeck(card, extraDeck.length, 'extra', 3)" />
     </div>
   </div>
-  <div class="flex flex-col gap-2 mt-3 dark:text-neutral-300">
+  <div class="mt-3 dark:text-neutral-300">
     <span>To Side Deck:</span>
-    <div class="flex gap-2">
-      <ButtonCTA variant="neutral-2" text-content="+ 1" class="sm:text-sm" :disabled="!isCardWithinLimit(card, 'side')"
-        @click="addCardToDeck(card, sideDeck.length, 'side')" />
-      <ButtonCTA variant="neutral-2" text-content="+ 2" class="sm:text-sm"
+    <div class="flex gap-2 mt-1">
+      <ButtonCTA variant="emerald" text-content="+ 1" aria-label="Add 1 copy"
+        :disabled="!isCardWithinLimit(card, 'side')" @click="addCardToDeck(card, sideDeck.length, 'side')" />
+      <ButtonCTA variant="emerald" text-content="+ 2" aria-label="Add 2 copies"
         :disabled="!isCardWithinLimit(card, 'side', 2)" @click="addCardToDeck(card, sideDeck.length, 'side', 2)" />
-      <ButtonCTA variant="neutral-2" text-content="+ 3" class="sm:text-sm"
+      <ButtonCTA variant="emerald" text-content="+ 3" aria-label="Add 3 copies"
         :disabled="!isCardWithinLimit(card, 'side', 3)" @click="addCardToDeck(card, sideDeck.length, 'side', 3)" />
     </div>
   </div>
