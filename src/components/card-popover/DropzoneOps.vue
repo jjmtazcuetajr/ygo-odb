3badfc0c9e96f39a789ab1560e51ce9f21836908
Michael Azcueta <azcuetajjmt@gmail.com>
2025-08-13 16:41:25 +0800

    add card transfer controls from extra to side deck

--- src/components/card-popover/DropzoneOps.vue
+++ src/components/card-popover/DropzoneOps.vue
@@ -17,23 +17,25 @@ const { mainDeck, extraDeck, sideDeck, getCardFrequency } = storeToRefs(deckStor
 const { isCardWithinLimit, addCardToDeck, removeCardFromDeck } = useDeckStore()
 
 /**
- * Transfer card copies from main deck to side deck
+ * Transfer card copies between deck types
  * @param num Number of card copies to transfer. Defaults to `1` copy
  */
-function moveFromMainToSideDeck(num: number = 1) {
-  const sameCardCount = deckStore.getCardFrequency(props.card.id, 'main')
-  const difference = EXTRA_AND_SIDE_DECK_LIMIT - sideDeck.value.length
+function crossdeckCardTransfer(from: Dropzone, num: number = 1) {
+  const sameCardCount = deckStore.getCardFrequency(props.card.id, from)
+  const difference = from === 'main' || from === 'extra' ? EXTRA_AND_SIDE_DECK_LIMIT - sideDeck.value.length : 0
 
   if (num === 1 && sideDeck.value.length < EXTRA_AND_SIDE_DECK_LIMIT) {
-    removeCardFromDeck(props.fromIndex, 'main')
+    removeCardFromDeck(props.fromIndex, from)
     addCardToDeck(props.card, sideDeck.value.length, 'side')
   } else if (num === 2 && (sameCardCount >= 2 && difference >= 2)) {
-    removeCardFromDeck(props.fromIndex, 'main', 2)
+    removeCardFromDeck(props.fromIndex, from, 2)
     addCardToDeck(props.card, sideDeck.value.length, 'side', 2)
   } else if (num === 3 && (sameCardCount === 3 && difference >= 3)) {
-    removeCardFromDeck(props.fromIndex, 'main', 3)
+    removeCardFromDeck(props.fromIndex, from, 3)
     addCardToDeck(props.card, sideDeck.value.length, 'side', 3)
   }
+
+  popoverClose()
 }
 
 const emit = defineEmits<{ 'handle-popover-close': [] }>()
@@ -73,25 +75,47 @@ function popoverClose() {
     </div>
   </div>
   <div class="mt-3 dark:text-neutral-300">
-    <span>Move to Side Deck:</span>
+    <span v-if="source === 'main' || source === 'extra'">Move to Side Deck:</span>
     <div class="flex gap-2 mt-1">
       <template v-if="source === 'main'">
         <ButtonCTA has-icon variant="neutral-2" aria-label="Move 1 Copy" :disabled="sideDeck.length === 15"
-          @click="[moveFromMainToSideDeck(), popoverClose()]">
+          @click="crossdeckCardTransfer('main')">
           <template #textWithIcon>
             <ArrowLeftRight :size="16" /> 1
           </template>
         </ButtonCTA>
         <ButtonCTA has-icon variant="neutral-2" aria-label="Move 2 Copies"
           :disabled="getCardFrequency(card.id, 'main') < 2 || EXTRA_AND_SIDE_DECK_LIMIT - sideDeck.length < 2"
-          @click="[moveFromMainToSideDeck(2), popoverClose()]">
+          @click="crossdeckCardTransfer('main', 2)">
           <template #textWithIcon>
             <ArrowLeftRight :size="16" /> 2
           </template>
         </ButtonCTA>
         <ButtonCTA has-icon variant="neutral-2" aria-label="Move 3 Copies"
           :disabled="getCardFrequency(card.id, 'main') < 3 || EXTRA_AND_SIDE_DECK_LIMIT - sideDeck.length < 3"
-          @click="[moveFromMainToSideDeck(3), popoverClose()]">
+          @click="crossdeckCardTransfer('main', 3)">
+          <template #textWithIcon>
+            <ArrowLeftRight :size="16" /> 3
+          </template>
+        </ButtonCTA>
+      </template>
+      <template v-else-if="source === 'extra'">
+        <ButtonCTA has-icon variant="neutral-2" aria-label="Move 1 Copy" :disabled="sideDeck.length === 15"
+          @click="crossdeckCardTransfer('extra')">
+          <template #textWithIcon>
+            <ArrowLeftRight :size="16" /> 1
+          </template>
+        </ButtonCTA>
+        <ButtonCTA has-icon variant="neutral-2" aria-label="Move 2 Copies"
+          :disabled="getCardFrequency(card.id, 'extra') < 2 || EXTRA_AND_SIDE_DECK_LIMIT - sideDeck.length < 2"
+          @click="crossdeckCardTransfer('extra', 2)">
+          <template #textWithIcon>
+            <ArrowLeftRight :size="16" /> 2
+          </template>
+        </ButtonCTA>
+        <ButtonCTA has-icon variant="neutral-2" aria-label="Move 3 Copies"
+          :disabled="getCardFrequency(card.id, 'extra') < 3 || EXTRA_AND_SIDE_DECK_LIMIT - sideDeck.length < 3"
+          @click="crossdeckCardTransfer('extra', 3)">
           <template #textWithIcon>
             <ArrowLeftRight :size="16" /> 3
           </template>
