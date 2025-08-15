2897350ce68c4a7aaa993c46f92bc19fedb95ffc
Michael Azcueta <azcuetajjmt@gmail.com>
2025-08-15 16:27:46 +0800

    refactor

--- src/components/card-popover/GridToDeck.vue
+++ src/components/card-popover/GridToDeck.vue
@@ -1,57 +1,91 @@
 <script setup lang="ts">
 import ButtonCTA from '../ButtonCTA.vue'
-import type { YGOCardData } from '@/utils/interfaces'
+import type { YGOCardData, Dropzone } from '@/utils/interfaces'
 import { useDeckStore } from '@/stores/deck'
 import { storeToRefs } from 'pinia'
 import { isMainDeckCard, isExtraDeckCard } from '@/utils/components'
 import { MAIN_DECK_LIMIT, EXTRA_AND_SIDE_DECK_LIMIT } from '@/utils/constants'
 
-defineProps<{ card: YGOCardData }>()
+const props = defineProps<{ card: YGOCardData }>()
 
 const { mainDeck, extraDeck, sideDeck } = storeToRefs(useDeckStore())
 const { isCardWithinLimit, addCardToDeck } = useDeckStore()
+
+/**
+ * Determine the disabled state of buttons
+ * @param from Deck type to add the card to
+ * @param num Number of card copies
+ */
+function handleDisabledState(to: Dropzone, num: number): boolean {
+  switch (to) {
+    case 'main':
+      return !isCardWithinLimit(props.card, to, num) || MAIN_DECK_LIMIT - mainDeck.value.length < num
+    case 'extra':
+      return !isCardWithinLimit(props.card, to, num) || EXTRA_AND_SIDE_DECK_LIMIT - extraDeck.value.length < num
+    case 'side':
+      return !isCardWithinLimit(props.card, to, num) || EXTRA_AND_SIDE_DECK_LIMIT - sideDeck.value.length < num
+    default:
+      break
+  }
+  return false
+}
+
+/**
+ * Add card/s to a type of deck
+ * @param to Deck type to add the card to
+ * @param num Number of card copies to add
+ */
+function handleAdd(to: Dropzone, num: number) {
+  switch (to) {
+    case 'main':
+      if (isCardWithinLimit(props.card, to, num) && MAIN_DECK_LIMIT - mainDeck.value.length >= num)
+        addCardToDeck(props.card, mainDeck.value.length, to, num)
+      break
+    case 'extra':
+      if (isCardWithinLimit(props.card, to, num) && EXTRA_AND_SIDE_DECK_LIMIT - extraDeck.value.length >= num)
+        addCardToDeck(props.card, extraDeck.value.length, to, num)
+      break
+    case 'side':
+      if (isCardWithinLimit(props.card, to, num) && EXTRA_AND_SIDE_DECK_LIMIT - sideDeck.value.length >= num)
+        addCardToDeck(props.card, sideDeck.value.length, to, num)
+      break
+    default:
+      break
+  }
+}
 </script>
 <template>
   <div v-if="isMainDeckCard(card.frameType)" class="dark:text-neutral-300">
     <span>To Main Deck:</span>
     <div class="flex gap-2 mt-1">
-      <ButtonCTA variant="emerald" text-content="+ 1" aria-label="Add 1 copy"
-        :disabled="!isCardWithinLimit(card, 'main') || mainDeck.length === MAIN_DECK_LIMIT"
+      <ButtonCTA variant="emerald" text-content="+ 1" aria-label="Add 1 copy" :disabled="handleDisabledState('main', 1)"
         @click="addCardToDeck(card, mainDeck.length, 'main')" />
       <ButtonCTA variant="emerald" text-content="+ 2" aria-label="Add 2 copies"
-        :disabled="!isCardWithinLimit(card, 'main', 2) || MAIN_DECK_LIMIT - mainDeck.length < 2"
-        @click="isCardWithinLimit(card, 'main', 2) && addCardToDeck(card, mainDeck.length, 'main', 2)" />
+        :disabled="handleDisabledState('main', 2)" @click="handleAdd('main', 2)" />
       <ButtonCTA variant="emerald" text-content="+ 3" aria-label="Add 3 copies"
-        :disabled="!isCardWithinLimit(card, 'main', 3) || MAIN_DECK_LIMIT - mainDeck.length < 3"
-        @click="isCardWithinLimit(card, 'main', 3) && addCardToDeck(card, mainDeck.length, 'main', 3)" />
+        :disabled="handleDisabledState('main', 3)" @click="handleAdd('main', 3)" />
     </div>
   </div>
   <div v-else-if="isExtraDeckCard(card.frameType)" class="dark:text-neutral-300">
     <span>To Extra Deck:</span>
     <div class="flex gap-2 mt-1">
       <ButtonCTA variant="emerald" text-content="+ 1" aria-label="Add 1 copy"
-        :disabled="!isCardWithinLimit(card, 'extra') || extraDeck.length === EXTRA_AND_SIDE_DECK_LIMIT"
-        @click="addCardToDeck(card, extraDeck.length, 'extra')" />
+        :disabled="handleDisabledState('extra', 1)" @click="addCardToDeck(card, extraDeck.length, 'extra')" />
       <ButtonCTA variant="emerald" text-content="+ 2" aria-label="Add 2 copies"
-        :disabled="!isCardWithinLimit(card, 'extra', 2) || EXTRA_AND_SIDE_DECK_LIMIT - extraDeck.length < 2"
-        @click="isCardWithinLimit(card, 'extra', 2) && addCardToDeck(card, extraDeck.length, 'extra', 2)" />
+        :disabled="handleDisabledState('extra', 2)" @click="handleAdd('extra', 2)" />
       <ButtonCTA variant="emerald" text-content="+ 3" aria-label="Add 3 copies"
-        :disabled="!isCardWithinLimit(card, 'extra', 3) || EXTRA_AND_SIDE_DECK_LIMIT - extraDeck.length < 3"
-        @click="isCardWithinLimit(card, 'extra', 3) && addCardToDeck(card, extraDeck.length, 'extra', 3)" />
+        :disabled="handleDisabledState('extra', 3)" @click="handleAdd('extra', 3)" />
     </div>
   </div>
   <div class="mt-3 dark:text-neutral-300">
     <span>To Side Deck:</span>
     <div class="flex gap-2 mt-1">
-      <ButtonCTA variant="emerald" text-content="+ 1" aria-label="Add 1 copy"
-        :disabled="!isCardWithinLimit(card, 'side') || sideDeck.length === EXTRA_AND_SIDE_DECK_LIMIT"
+      <ButtonCTA variant="emerald" text-content="+ 1" aria-label="Add 1 copy" :disabled="handleDisabledState('side', 1)"
         @click="addCardToDeck(card, sideDeck.length, 'side')" />
       <ButtonCTA variant="emerald" text-content="+ 2" aria-label="Add 2 copies"
-        :disabled="!isCardWithinLimit(card, 'side', 2) || EXTRA_AND_SIDE_DECK_LIMIT - sideDeck.length < 2"
-        @click="isCardWithinLimit(card, 'side', 2) && addCardToDeck(card, sideDeck.length, 'side', 2)" />
+        :disabled="handleDisabledState('side', 2)" @click="handleAdd('side', 2)" />
       <ButtonCTA variant="emerald" text-content="+ 3" aria-label="Add 3 copies"
-        :disabled="!isCardWithinLimit(card, 'side', 3) || EXTRA_AND_SIDE_DECK_LIMIT - sideDeck.length < 3"
-        @click="isCardWithinLimit(card, 'side', 3) && addCardToDeck(card, sideDeck.length, 'side', 3)" />
+        :disabled="handleDisabledState('side', 3)" @click="handleAdd('side', 3)" />
     </div>
   </div>
 </template>
