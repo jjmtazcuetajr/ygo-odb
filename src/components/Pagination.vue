6e2e4a28371e3d622f7192e1fa8875d81bcb613a
Michael Azcueta <azcuetajjmt@gmail.com>
2025-08-30 16:46:52 +0800

    standardize pagination button colors

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -14,28 +14,28 @@ const { prev, next, toFirst, toLast } = store
     <nav class="flex overflow-x-auto">
       <div class="flex items-center gap-1 p-1 dark:text-white">
         <button type="button" aria-label="First Page" @click="toFirst" :disabled="currentPage === 1"
-          class="p-2 cursor-pointer rounded-lg flex items-center justify-center bg-transparent hover:bg-neutral-300 active:bg-neutral-400 dark:hover:bg-neutral-700 dark:active:bg-neutral-600 transition-[background-color,color] duration-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:active:bg-neutral-300 dark:disabled:active:bg-neutral-700">
+          class="p-2 cursor-pointer rounded-lg flex items-center justify-center bg-transparent hover:bg-neutral-300 active:bg-neutral-400 dark:hover:bg-neutral-700 dark:active:bg-neutral-600 transition-[background-color,color] duration-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-transparent">
           <ChevronsLeft :size="16" />
         </button>
         <button type="button" aria-label="Previous Page" @click="prev" :disabled="currentPage === 1"
-          class="p-2 cursor-pointer rounded-lg flex items-center justify-center bg-transparent hover:bg-neutral-300 active:bg-neutral-400 dark:hover:bg-neutral-700 dark:active:bg-neutral-600 transition-[background-color,color] duration-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:active:bg-neutral-300 dark:disabled:active:bg-neutral-700">
+          class="p-2 cursor-pointer rounded-lg flex items-center justify-center bg-transparent hover:bg-neutral-300 active:bg-neutral-400 dark:hover:bg-neutral-700 dark:active:bg-neutral-600 transition-[background-color,color] duration-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-transparent">
           <ChevronLeft :size="16" />
         </button>
         <DialogModal usage="pagination">
           <template #trigger>
             <button type="button" title="Jump to Page"
               :aria-label="`Page ${currentPage} of ${totalPages}. Jump to a certain page?`"
-              class="p-1 whitespace-nowrap cursor-pointer rounded-md bg-neutral-300 hover:bg-neutral-400/70 active:bg-neutral-400 dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:active:bg-neutral-500 transition-[background-color,color] duration-200">
+              class="p-1 whitespace-nowrap cursor-pointer rounded-md text-white bg-neutral-500 hover:bg-neutral-600 active:bg-neutral-700 transition-[background-color] duration-200">
               {{ `${currentPage} / ${totalPages}` }}
             </button>
           </template>
         </DialogModal>
         <button type="button" aria-label="Next Page" @click="next" :disabled="currentPage === totalPages"
-          class="p-2 cursor-pointer rounded-lg flex items-center justify-center bg-transparent hover:bg-neutral-300 active:bg-neutral-400 dark:hover:bg-neutral-700 dark:active:bg-neutral-600 transition-[background-color,color] duration-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:active:bg-neutral-300 dark:disabled:active:bg-neutral-700">
+          class="p-2 cursor-pointer rounded-lg flex items-center justify-center bg-transparent hover:bg-neutral-300 active:bg-neutral-400 dark:hover:bg-neutral-700 dark:active:bg-neutral-600 transition-[background-color,color] duration-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-transparent">
           <ChevronRight :size="16" />
         </button>
         <button type="button" aria-label="Last Page" @click="toLast" :disabled="currentPage === totalPages"
-          class="p-2 cursor-pointer rounded-lg flex items-center justify-center bg-transparent hover:bg-neutral-300 active:bg-neutral-400 dark:hover:bg-neutral-700 dark:active:bg-neutral-600 transition-[background-color,color] duration-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:active:bg-neutral-300 dark:disabled:active:bg-neutral-700">
+          class="p-2 cursor-pointer rounded-lg flex items-center justify-center bg-transparent hover:bg-neutral-300 active:bg-neutral-400 dark:hover:bg-neutral-700 dark:active:bg-neutral-600 transition-[background-color,color] duration-200 disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-transparent">
           <ChevronsRight :size="16" />
         </button>
       </div>
