7cb8609432febc226023fa34a554fca1367a2263
Michael Azcueta <azcuetajjmt@gmail.com>
2025-10-28 14:07:42 +0800

    update logo

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -34,7 +34,7 @@ onBeforeMount(() => {
     <nav class="flex justify-between items-center text-neutral-800 dark:text-neutral-300">
       <RouterLink to="/" class="focus-visible:outline-offset-2">
         <div class="flex items-center font-medium text-xl hover:opacity-70 transition-[opacity,color] duration-400">
-          <img alt="Vue logo" class="mr-3" src="@/assets/logo.svg" width="30" height="20" />YGO ODB
+          <img alt="Vue logo" class="mr-1" src="@/assets/logo.webp" width="30" />YGO ODB
         </div>
       </RouterLink>
       <div class="flex items-center divide-x-1 divide-neutral-300 dark:divide-neutral-700">
