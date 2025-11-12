c18432facf6b9f58c9dad5efafc629fb7845ce3d
Michael Azcueta <azcuetajjmt@gmail.com>
2025-11-12 14:32:06 +0800

    put a max date to the date filters

--- src/components/general-purpose/DateInput.vue
+++ src/components/general-purpose/DateInput.vue
@@ -6,11 +6,13 @@ defineProps<{
 }>()
 
 const model = defineModel<string>({ required: true })
+
+const maxDate = new Date().toISOString().split('T')[0]
 </script>
 <template>
   <div>
     <label :for="id">{{ labelText }}</label>
-    <input :id="id" type="date" :min="min" v-model="model"
+    <input :id="id" type="date" :min="min" :max="maxDate" v-model="model"
       class="text-xs sm:text-base px-1 py-0.5 border rounded-md border-neutral-500 bg-neutral-200 dark:bg-neutral-800 scheme-light dark:scheme-dark">
   </div>
 </template>
