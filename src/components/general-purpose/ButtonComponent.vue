835e71ce98f843118ec1578ddb43f68d102222c6
Michael Azcueta <azcuetajjmt@gmail.com>
2026-01-09 11:45:11 +0800

    fix eslint error and prettify code in ButtonComponent

--- src/components/general-purpose/ButtonComponent.vue
+++ src/components/general-purpose/ButtonComponent.vue
@@ -1,29 +1,36 @@
 <script setup lang="ts">
+import type { Slot } from 'vue'
+
 const props = defineProps<{
-  variant: 'emerald' | 'neutral' | 'red' | 'sky',
-  hasIcon?: boolean,
-  onlyInMobile?: boolean,
+  variant: 'emerald' | 'neutral' | 'red' | 'sky'
+  hasIcon?: boolean
+  onlyInMobile?: boolean
   textContent?: string
 }>()
 
 const slots = defineSlots<{
-  textWithIcon?: () => any
+  textWithIcon?: () => Slot
 }>()
 
 const hasTextWithIconSlot = !!slots.textWithIcon
 
 const buttonVariants: Record<string, string> = {
-  'emerald': 'bg-emerald-700 hover:bg-emerald-800 active:bg-emerald-900 disabled:hover:bg-emerald-700 disabled:active:bg-emerald-700',
-  'neutral': 'bg-neutral-500 hover:bg-neutral-600 active:bg-neutral-700 disabled:hover:bg-neutral-500 disabled:active:bg-neutral-500',
-  'red': 'bg-red-700 hover:bg-red-800 active:bg-red-900 disabled:hover:bg-red-700 disabled:active:bg-red-700',
-  'sky': 'bg-sky-700 hover:bg-sky-800 active:bg-sky-900 disabled:hover:bg-sky-700 disabled:active:bg-sky-700'
+  emerald:
+    'bg-emerald-700 hover:bg-emerald-800 active:bg-emerald-900 disabled:hover:bg-emerald-700 disabled:active:bg-emerald-700',
+  neutral:
+    'bg-neutral-500 hover:bg-neutral-600 active:bg-neutral-700 disabled:hover:bg-neutral-500 disabled:active:bg-neutral-500',
+  red: 'bg-red-700 hover:bg-red-800 active:bg-red-900 disabled:hover:bg-red-700 disabled:active:bg-red-700',
+  sky: 'bg-sky-700 hover:bg-sky-800 active:bg-sky-900 disabled:hover:bg-sky-700 disabled:active:bg-sky-700',
 }
 const withIcon = props.hasIcon ? 'flex place-items-center gap-1' : ''
 const mobileOnly = props.onlyInMobile ? 'lg:hidden' : ''
 </script>
 <template>
-  <button type="button" :class="`${buttonVariants[variant]} ${withIcon} ${mobileOnly}`"
-    class="px-2 py-1 rounded-md cursor-pointer text-xs sm:text-base text-white transition-[background-color] duration-200 disabled:opacity-40 disabled:cursor-not-allowed">
+  <button
+    type="button"
+    :class="`${buttonVariants[variant]} ${withIcon} ${mobileOnly}`"
+    class="px-2 py-1 rounded-md cursor-pointer text-xs sm:text-base text-white transition-[background-color] duration-200 disabled:opacity-40 disabled:cursor-not-allowed"
+  >
     <slot name="textWithIcon" v-if="hasTextWithIconSlot"></slot>
     <template v-else>{{ textContent }}</template>
   </button>
