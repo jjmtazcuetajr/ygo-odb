e0307ec0fe520278ab2a227aaae5a2cfa2b0ae44
Michael Azcueta <azcuetajjmt@gmail.com>
2026-01-15 21:52:26 +0800

    prettify some loaders

--- src/components/loaders/CardDetailsMobileLoader.vue
+++ src/components/loaders/CardDetailsMobileLoader.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="flex flex-col w-full gap-2">
-    <div class="w-full h-5 sm:h-6 rounded-md animate-pulse bg-neutral-300 dark:bg-neutral-700"></div>
+    <div
+      class="w-full h-5 sm:h-6 rounded-md animate-pulse bg-neutral-300 dark:bg-neutral-700"
+    ></div>
     <div class="flex flex-col grow justify-around">
-      <div class="w-20 h-6 sm:h-8 rounded-md animate-pulse bg-neutral-300 dark:bg-neutral-700"></div>
+      <div
+        class="w-20 h-6 sm:h-8 rounded-md animate-pulse bg-neutral-300 dark:bg-neutral-700"
+      ></div>
       <div class="flex gap-4">
-        <div class="w-20 h-6 sm:h-8 rounded-md animate-pulse bg-neutral-300 dark:bg-neutral-700"></div>
-        <div class="w-20 h-6 sm:h-8 rounded-md animate-pulse bg-neutral-300 dark:bg-neutral-700"></div>
+        <div
+          class="w-20 h-6 sm:h-8 rounded-md animate-pulse bg-neutral-300 dark:bg-neutral-700"
+        ></div>
+        <div
+          class="w-20 h-6 sm:h-8 rounded-md animate-pulse bg-neutral-300 dark:bg-neutral-700"
+        ></div>
       </div>
     </div>
   </div>
